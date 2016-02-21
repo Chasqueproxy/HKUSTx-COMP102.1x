833d49b2c121fe9c5aff90af5600b7f146f9f967
Soumil Mandal <soumil.mandal@gmail.com>
2016-02-21 17:39:29 +0530

    Create swapCards.java

---
+++ Module-05/Lab-05/swapCards.java
@@ -0,0 +1,12 @@
+private void swapCards(int cardAIndex, int cardBIndex) {
+
+        // Please write your code after this line
+				Card temp = cards[ cardAIndex ]; //save current card in temp location
+        cards [ cardAIndex ] = cards [ cardBIndex ];
+        cards [ cardBIndex ] = temp;
+
+
+
+
+
+}
