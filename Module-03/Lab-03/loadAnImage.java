6cc01a8cd3da2a76f53b0dcceb5d74153622ef52
Soumil Mandal <soumil.mandal@gmail.com>
2016-02-21 17:28:22 +0530

    Create loadAnImage.java

---
+++ Module-03/Lab-03/loadAnImage.java
@@ -0,0 +1,13 @@
+public void loadAnImage() {
+
+        // Please write your code after this line
+        ColorImage image = new ColorImage();
+        Canvas canvas = new Canvas(image.getWidth(), image.getHeight());
+        canvas.add(image);
+
+
+
+
+
+
+}
