ac6a61109a11069fbcef53a90893f56ccfda429b
Soumil Mandal <soumil.mandal@gmail.com>
2016-02-21 17:22:18 +0530

    Create calculateTriangleArea.java

---
+++ Module-02/Lab-02/calculateTriangleArea.java
@@ -0,0 +1,15 @@
+public static void calculateTriangleArea() {
+
+        // Please write your code after this line
+        double width=0.0;
+        double height=0.0;
+        System.out.print("Enter the width of the triangle: ");width=IO.inputDouble();
+        System.out.print("Enter the height of the triangle: ");height=IO.inputDouble();
+        System.out.print("The triangle area = "+(width*height)/2.0);
+
+
+
+
+
+
+}
