07e4978e8ad02823dbe56f5b09d229ab3d51e1ab
Soumil Mandal <soumil.mandal@gmail.com>
2016-02-21 17:20:08 +0530

    Create multiply.java

---
+++ Module-02/Lab-02/multiply.java
@@ -0,0 +1,14 @@
+public static void multiply() {
+
+        // Please write your code after this line
+        int x=0;
+        int y=0;
+        System.out.print("Enter an integer, x: ");x=IO.inputInteger();
+        System.out.print("Enter an integer, y: ");y=IO.inputInteger();
+        System.out.print("Answer = "+(x*y));
+
+
+
+
+
+}
